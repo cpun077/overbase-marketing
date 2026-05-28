fc1b7299103b0c9de9d96ab3d158b2c6ed291da2
Julien Newman <newmanjulien@users.noreply.github.com>
2026-05-28 19:43:35 -0400

    tweaks

--- src/lib/home/features/opportunity-email/OpportunityEmailPreview.svelte
+++ src/lib/home/features/opportunity-email/OpportunityEmailPreview.svelte
@@ -5,7 +5,7 @@
 <div class="min-h-0 flex-1 overflow-auto bg-white">
   <div>
     <pre
-      class="whitespace-pre-wrap px-[17px] pb-[4px] pt-[20px] text-[13px] font-normal leading-[1.65] tracking-normal text-stone-400 md:px-[19px] md:pt-[22px]"
+      class="whitespace-pre-wrap px-[17px] pb-[4px] pt-[20px] text-[13px] font-normal leading-[1.65] tracking-normal text-stone-600 md:px-[19px] md:pt-[22px]"
     ><code>{email}</code></pre>
     <img class="ml-[17px] mt-[26px] block w-[126px] md:ml-[19px]" src="/logo_full.png" alt="Overbase" />
   </div>
